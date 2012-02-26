f8554555b6eb8e9ad4c9a93cacbbe53f785f7bca
Tetsuya Morimoto <tetsuya dot morimoto at gmail dot com>
2012-02-26 20:09:09 +0900

    refactoring the package definition

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from distutils.core import setup
 
+VERSION = '0.2-dev'
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: System :: Archiving',
+    'Topic :: System :: Software Distribution',
+]
+
 setup(
     name = 'python-archive',
-    version = '0.2-dev',
+    version = VERSION,
+    classifiers = CLASSIFIERS,
     author = 'Gary Wilson Jr.',
     author_email = 'gary.wilson@gmail.com',
     packages = ['archive', 'archive.test'],
     url = 'https://github.com/gdub/python-archive',
-    license = 'LICENSE.txt',
+    license = 'MIT License',
     description = ('Simple library that provides a common interface for'
                    ' extracting zip and tar archives.'),
     long_description = open('README.txt').read(),
